8e8c2fcb625c7a2a9e9b82c27cbcfd4008c8d31a
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-04-21 11:35:27 -0500

    Add SongLink Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Emotions from "./pages/Emotions";
+import SongLink from "./components/Song Link/SongLink";
+import { emotionsObject, hifyObject } from "./components/Song Link/Data";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/emotions" element={<Emotions />} />
+        <Route path="/emotions" element={<SongLink {...emotionsObject} />} />
+        <Route path="/hearitfromyou" element={<SongLink {...hifyObject} />} />
       </Routes>
     </Router>
   );
